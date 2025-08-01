94cb13950c17402f1a5768379cc6750eb39b85a6
xScheco <scheco13@gmail.com>
2025-08-01 18:07:37 +0200

    add translation for home section

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,19 +1,22 @@
+import { useTranslation } from "react-i18next";
 import placeholder from "../../assets/placeholder_img.jpg";
 
 export const Home = () => {
+  const { t } = useTranslation();
+
   return (
     <section
       id="home"
       className="w-full px-4 h-[46.875rem] bg-[radial-gradient(circle_at_top_right,_#ffe1fb,_#e1f4ff,_#fef6e4)] mt-14 pt-24"
     >
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-12 font-overpass">
-        <div className="text-center md:text-left max-w-lg">
-          <h2 className="md:text-7xl text-4xl font-bold mb-4 text-gray-900">
-            Hello, I'm Marco Scherf
+        <div className="text-center md:text-left max-w-xl">
+          <h2 className="flex flex-col md:text-7xl text-4xl font-bold mb-4 text-gray-900">
+            {t("home.title")}
+            <span> Marco Scherf</span>
           </h2>
           <span className="text-base text-gray-600">
-            I am a very focused and inquisitive person with high standards for
-            my projects.
+            {t("home.title_description")}
           </span>
         </div>
         <div>
